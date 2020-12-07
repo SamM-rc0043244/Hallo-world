f3935da0684bd69f644822b4dcb72e617db56285
Samuel McCallum <RC0043244@student.riversidecollege.ac.uk>
2020-12-07 15:03:44 +0000

    BYE 2.0

--- Hallo world/Form1.Designer.cs
+++ Hallo world/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(66, 49);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Goodbye";
+            this.button1.Text = "Hallo";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
--- Hallo world/Form1.cs
+++ Hallo world/Form1.cs
@@ -24,7 +24,7 @@ namespace Hallo_world
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hallo" + hallo.Text);
+            MessageBox.Show("BYE" + hallo.Text);
         }
     }
 }
